bc650463518a540d6096046f9fcf5d2bc563e4ad
Michael Anderson <Micmail566@gmail.com>
2017-01-12 00:18:10 -0500

    Need to stop player when they go in the same space twice.

--- Tic Tac Toe 2/Program.cs
+++ Tic Tac Toe 2/Program.cs
@@ -28,46 +28,73 @@ namespace Tic_Tac_Toe_2
                     string player1 = Console.ReadLine();
                     Console.WriteLine("Player Two Enter Name");
                     string player2 = Console.ReadLine();
-                    string[] board = { "_", "_", "_", "_", "_", "_", "_", "_", "_" };
+                    string[] board = { "______0", "______1", "______2", "______3", "______4", "______5", "______6", "______7", "______8" };
 
                     {
                         string player = player1;
                         int count = 1;
                         bool stopBoard = true;
                         while (stopBoard)
-
-                            if (player == player1)
+                            if ((board[0].Contains("X")) && (board[1].Contains("X")) && (board[2].Contains("X")) ||
+                                (board[3].Contains("X")) && (board[4].Contains("X")) && (board[5].Contains("X")) ||
+                                (board[6].Contains("X")) && (board[7].Contains("X")) && (board[8].Contains("X")) ||
+                                (board[0].Contains("X")) && (board[4].Contains("X")) && (board[8].Contains("X")) ||
+                                (board[2].Contains("X")) && (board[4].Contains("X")) && (board[6].Contains("X")) ||
+                                (board[1].Contains("X")) && (board[4].Contains("X")) && (board[7].Contains("X")) ||
+                                (board[0].Contains("X")) && (board[3].Contains("X")) && (board[6].Contains("X")) ||
+                                (board[2].Contains("X")) && (board[5].Contains("X")) && (board[8].Contains("X")))
+                            //0 1 2
+                            //3 4 5
+                            //6 7 8
                             {
-                                
+                                Console.WriteLine("Winner X");
+                                stopBoard = false;
+                            }
+                            else if ((board[0].Contains("O")) && (board[1].Contains("O")) && (board[2].Contains("O")) ||
+                                (board[3].Contains("O")) && (board[4].Contains("O")) && (board[5].Contains("O")) ||
+                                (board[6].Contains("O")) && (board[7].Contains("O")) && (board[8].Contains("O")) ||
+                                (board[0].Contains("O")) && (board[4].Contains("O")) && (board[8].Contains("O")) ||
+                                (board[2].Contains("O")) && (board[4].Contains("O")) && (board[6].Contains("O")) ||
+                                (board[1].Contains("O")) && (board[4].Contains("O")) && (board[7].Contains("O")) ||
+                                (board[0].Contains("O")) && (board[3].Contains("O")) && (board[6].Contains("O")) ||
+                                (board[2].Contains("O")) && (board[5].Contains("O")) && (board[8].Contains("O")))
+                            {
+                                Console.WriteLine("Winner O");
+                                stopBoard = false;
+
+                            }
+                            else if
+                               (player == player1)
+                            {
+
                                 BuildBoard(board); // Calls method and rebuilds the board.
                                 Console.WriteLine($"{player1}'s Turn");
                                 int entry = int.Parse(Console.ReadLine());
-                                board[entry] = "X";
-                                Console.WriteLine("To The Death!");
+                                board[entry] = "___X___";
                                 Console.WriteLine("Turn" + count);
                                 count++;
                                 player = player2;
+
                             }
-                        
-                            else if ( player == player2 )
+
+                            else if (player == player2)
                             {
-                                Console.Clear();
+
                                 BuildBoard(board); // Calls method and rebuilds the board.
                                 Console.WriteLine($"{player2}'s Turn");
                                 int entry2 = int.Parse(Console.ReadLine());
-                                board[entry2] = "O";
-                                Console.WriteLine("To The Death!");
+                                board[entry2] = "___O___";
                                 Console.WriteLine("Turn" + count);
                                 player = player1;
 
                             }
 
-                           
 
 
-                        
-                       
-                        
+
+
+
+
                     }
                 }
 
